4f329f6b4898a9138a086db730e9e246dc730832
Grade <aborecevskij94@gmail.com>
2024-01-25 20:49:32 +0200

    Updated

--- src/Scrapper/auto_runner.py
+++ src/Scrapper/auto_runner.py
@@ -1,3 +1,11 @@
+import io
+
+from src.setup import setup_django
+
+setup_django()
+
+from django.core.management import call_command
+
 import logging
 import os
 import time
@@ -15,14 +23,27 @@ class Runner:
 
     @staticmethod
     def job():
+        logging.info("Running job")
+        Runner.dump_data()
         os.system("python D:\\AutoRiaScrapper\\src\\Scrapper\\main.py")
+
+    @staticmethod
+    def dump_data():
+        date = time.strftime("%Y-%m-%d")
+        root_dir = os.path.dirname(os.path.abspath(__file__))
+        path = os.path.join(root_dir, f"../../dumps")
+        with io.open(f"{path}/{date}.json", "w", encoding='utf-8') as file:
+            call_command("dumpdata", "ticket", stdout=file, format='json')
 
     def run(self):
         schedule.every().day.at(self.time).do(self.job)
 
         while True:
-            schedule.run_pending()
-            time.sleep(30)
+            try:
+                schedule.run_pending()
+                time.sleep(30)
+            except Exception as e:
+                logging.error(e)
 
 
 if __name__ == '__main__':
